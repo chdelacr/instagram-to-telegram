d5d1da659ec36d1b34fef5c1b9d6ee0978c09455
Christian de la Cruz <chrisdlc119@outlook.com>
2023-05-03 02:06:09 -0600

    Include code and post date in checkpoint file

--- telegram_handler.py
+++ telegram_handler.py
@@ -6,11 +6,10 @@ from datetime import datetime
 async def send_posts_to_channel(posts):
     print("Starting Telegram bot...")
     bot = telegram.Bot(os.getenv("TELEGRAM_BOT_TOKEN"))
-
-    print("Reading pks of previous Instagram posts sent to Telegram channel...")
-    sent_posts = set()
     
+    print("Reading identifier of previous Instagram posts sent to Telegram channel...")
     print("Connecting to SFTP...")
+    sent_posts = set()
     cnopts = pysftp.CnOpts()
     cnopts.hostkeys = None
 
@@ -29,31 +28,27 @@ async def send_posts_to_channel(posts):
                 with sftp.open(filename, 'r') as f:
                     sent_posts = set([line.strip() for line in f.readlines()])
 
-    print("Sending only new posts to Telegram channel...")
+    print("Sending only new posts (only picture, not part of albums) to Telegram channel...")
     channel_id = os.getenv("TELEGRAM_CHANNEL_ID")
     for post in posts:
-        if post.pk not in sent_posts:
-            print("Posting only Instagram picture (not part of albums)...")
-            if post.media_type == 1:
-                print("Getting Instagram picture URL...")
-                thumbnail_url = post.thumbnail_url
+        if f'{post.pk}_{post.code}_{post.taken_at}' not in sent_posts and post.media_type == 1:
+            print("Sending picture and caption to Telegram channel...")
+            thumbnail_url = post.thumbnail_url
+            caption = post.caption_text
+            await bot.send_photo(chat_id=channel_id, photo=thumbnail_url, caption=caption)
 
-                print("Sending picture and caption to Telegram channel...")
-                caption = post.caption_text
-                await bot.send_photo(chat_id=channel_id, photo=thumbnail_url, caption=caption)
+            sent_posts.add(f'{post.pk}_{post.code}_{post.taken_at}')
 
-                sent_posts.add(post.pk)
-
-    print("Saving pks of new posts (if any) sent to Telegram channel...")
+    print("Saving identifier of new posts (if any) sent to Telegram channel...")
     with pysftp.Connection(sftp_server, username=sftp_username, password=sftp_password, cnopts=cnopts) as sftp:
         with sftp.cd(sftp_path):
             with sftp.open(filename, 'w') as f:
                 f.write('\n'.join(sent_posts))
 
+            print("Deleting files in SFTP that are older than 7 days...")
             for file in sftp.listdir():
                 if file.startswith('sent_posts_'):
                     file_path = f'{sftp_path}/{file}'
                     mtime = sftp.stat(file_path).st_mtime
-                    print("Deleting files in SFTP that are older than 7 days...")
                     if (datetime.now() - datetime.fromtimestamp(mtime)).days > 7:
                         sftp.remove(file_path)
