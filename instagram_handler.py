f47714bea1a52972f2bb69c66953e91216208cf6
Christian de la Cruz <chrisdlc119@outlook.com>
2023-05-03 09:39:09 -0600

    Added support for video and album posts
    * Album support has been temporarily disabled since Telegram is not showing captions properly

--- instagram_handler.py
+++ instagram_handler.py
@@ -2,7 +2,7 @@ def get_latest_posts(cl, username):
     print("Getting id from Instagram username...")
     user_id = cl.user_id_from_username(username)
     
-    print("Getting last 10 posts from Instagram account...")
+    print("Getting last 10 posts from Instagram...")
     posts = cl.user_medias(user_id, 10)
 
     return posts
--- telegram_handler.py
+++ telegram_handler.py
@@ -8,13 +8,32 @@ async def send_posts_to_channel(posts):
 
     # Get sent posts from checkpoint file
     sent_posts = sftp_utils('r')
+
     channel_id = os.getenv("TELEGRAM_CHANNEL_ID")
     for post in posts:
-        if f'{post.pk}_{post.code}_{post.taken_at}' not in sent_posts and post.media_type == 1:
-            print("Sending only new posts (only picture, not part of albums) to Telegram channel...")
-            thumbnail_url = post.thumbnail_url
-            caption = post.caption_text
-            await bot.send_photo(chat_id=channel_id, photo=thumbnail_url, caption=caption)
+        if f'{post.pk}_{post.code}_{post.taken_at}' not in sent_posts:
+            if post.media_type == 1:
+                print("Sending new picture post to Telegram channel...")
+                photo = post.thumbnail_url
+                caption = post.caption_text
+                await bot.send_photo(chat_id=channel_id, photo=photo, caption=caption)
+            if post.media_type == 2:
+                print("Sending new video post to Telegram channel...")
+                video = post.video_url
+                caption = post.caption_text
+                await bot.send_video(chat_id=channel_id, video=video, caption=caption)
+            # Disabling album handling since Telegram isn't showing captions properly
+            """elif post.media_type == 8:
+                print("Sending new album post to Telegram channel...")
+                media_group = []
+                caption = post.caption_text
+                for p in post.resources:
+                    if p.media_type == 1:
+                        media_group.append(telegram.InputMediaPhoto(p.thumbnail_url, caption=caption))
+                    elif p.media_type == 2:
+                        media_group.append(telegram.InputMediaVideo(p.video_url, caption=caption))
+                        
+                await bot.send_media_group(chat_id=channel_id, media=media_group)"""
 
             sent_posts.add(f'{post.pk}_{post.code}_{post.taken_at}')
 
